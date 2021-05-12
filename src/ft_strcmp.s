1babebc67def89e4f2c486c0d536284b24625eec
GGWP-grats <grats420@gmail.com>
2021-05-12 14:28:26 +0300

    strcmp

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -1,17 +1,21 @@
 section .text
 global	ft_strcmp
+
 ft_strcmp:
-			test	rdi, rdi;
-			jz		.end;
-			test	rsi, rsi;
-			jz		.end;
-.loop:		
-			cmp 	byte [rsi], 0x00;
-			je		.end;
-			cmpsb;
-			jne		.end;
-			jmp		.loop;
-.end:
-			mov   	rax, [rsi];
-			sub		rax, [rdi];	
-			ret;
+			push		rcx;				save rcx
+			xor			rcx, rcx;			i = 0;
+			dec			rcx;				i--;
+.loop		inc			rcx;				i++;
+			mov			al,	[rdi + rcx];	al = s1[i]
+			mov			ah, [rsi + rcx];	ah = s2[i]
+			cmp			al, ah;				if (ah != al)
+			jne			.end;					goto .end
+			cmp			al, 0;				if (al == 0)
+			je			.end;					goto .end
+			jmp			.loop;				else goto .loop
+.end		xor			rcx, rcx;			rcx = 0
+			mov			cl, ah;				cl = s2[i]
+			and			rax, 0xff;			zero all beside s1[i]
+			sub			rax, rcx;			return(s1[i] - s2[i])
+			pop			rcx;				get rcx back;
+			ret;							ret rax
